fe4d0ddb0938ae24e4087b4c34d2435ae8c6df9e
Nikilesh Raj <nikileshnatraj@gmail.com>
2023-12-26 18:18:34 +0530

    modified for conditional tailwin with new class names

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,4 @@
-export default function CustomInput({ label, invalid, ...props }) {
+export default function Input({ label, invalid, ...props }) {
   let labelClasses = "block mb-2 text-xs font-bold tracking-wide uppercase ";
   let inputClasses =
     "w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow ";
